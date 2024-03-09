3e955b8236b95d0562ad790e852d336f14bbec95
Ashish Kumar Mohanty <ashishkumarmohanty7797@gmail.com>
2024-03-10 00:05:17 +0530

    add docker to the server

--- client/ecomm/src/components/AddRemoveFromCartButton.tsx
+++ client/ecomm/src/components/AddRemoveFromCartButton.tsx
@@ -15,16 +15,16 @@ const AddRemoveFromCartButton = ({
     <div className="w-full rounded-lg border-2 border-[#15F5B] flex items-center justify-between h-[40px] text-secondary">
       <button
         className="w-[100%] border-r border-r-[#15F5B] h-full"
-        onClick={() => addFunction()}
+        onClick={() => removeFunction()}
       >
-        +
+        -
       </button>
       <span className="w-[100%] text-secondary text-center">{value}</span>
       <button
         className="w-[100%] border-l border-l-[#15F5B] h-full"
-        onClick={() => removeFunction()}
+        onClick={() => addFunction()}
       >
-        -
+        +
       </button>
     </div>
   );
